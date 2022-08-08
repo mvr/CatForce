0567ae2f68bacfa5f8854555d008207f77cedc75
mvr <mitchell.v.riley@gmail.com>
2022-08-08 12:51:03 +1000

    More Convolve special cases

--- LifeAPI.h
+++ LifeAPI.h
@@ -776,6 +776,29 @@ public:
       case (1 << 6) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
       case (1 << 7) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
       case (1 << 8) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 9) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 10) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 11) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 12) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 13) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 14) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 15) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 16) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 17) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 18) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 19) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 20) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 21) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 22) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 23) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 24) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 25) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 26) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 27) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 28) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 29) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1 << 30) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
+      case (1ULL << 31) - 1: ConvolveInner(result, doubledother, x, 64-j, postshift); break;
       default:           ConvolveInner(result, doubledother, x, 64-j, postshift); break;
       }
     }
