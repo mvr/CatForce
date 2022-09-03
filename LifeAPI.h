15dc00da3568908d5f108f6375062f5f971ac1a0
mvr <mitchell.v.riley@gmail.com>
2022-09-03 13:01:06 +0400

    Revert "Use min and max in Convolve"
    This reverts commit 3c501fc8e899ee8c28e89d140df61d7f33679704.

--- LifeAPI.h
+++ LifeAPI.h
@@ -672,7 +672,7 @@ public:
     memcpy(doubledother,     other.state, N * sizeof(uint64_t));
     memcpy(doubledother + N, other.state, N * sizeof(uint64_t));
 
-    for (unsigned j = min; j <= max; j++) {
+    for (unsigned j = 0; j < N; j++) {
       unsigned k = 64-j;
       uint64_t x = state[j];
 
